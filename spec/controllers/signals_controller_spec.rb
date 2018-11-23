a28bb8465c67a63cf893963b2e5b2baee29f2b1f
Matthias Viehweger <viehweger@puzzle.ch>
2018-11-23 20:33:05 +0100

    Repair setup of request

--- spec/controllers/signals_controller_spec.rb
+++ spec/controllers/signals_controller_spec.rb
@@ -3,39 +3,31 @@
 require 'rails_helper'
 
 RSpec.describe SignalsController, type: :controller do # rubocop:disable Metrics/BlockLength,Metrics/LineLength
-  before do
-    Fabricate(:heartbeat, application: 'hitobito', token: 'supertoken',
-                          last_signal_at: 11.hours.ago, last_signal_ok: false)
-  end
-
+  let(:app_name) { 'hitobito' }
+  let(:app_token) { 'supertoken' }
   let(:heartbeat) do
-    Heartbeat.find_by(application: 'hitobito', token: 'supertoken')
+    Heartbeat.find_by(application: app_name, token: app_token)
   end
-
   let(:valid_attributes) do
-    {
-      status: %w[ok].sample
-    }.stringify_keys.merge(valid_session)
+    { status: 'ok' }.stringify_keys.merge(valid_session)
   end
-
   let(:invalid_attributes) do
-    {
-      status: 'TX'
-    }.stringify_keys.merge(valid_session)
+    { status: 'TX' }.stringify_keys.merge(valid_session)
+  end
+  let(:valid_session) do
+    { application: app_name, token: app_token }.stringify_keys
   end
 
-  let(:valid_session) do
-    {
-      application: heartbeat.application,
-      token: heartbeat.token
-    }.stringify_keys
+  before :example do
+    Fabricate(:heartbeat, application: app_name, token: app_token,
+                          last_signal_at: 11.hours.ago, last_signal_ok: false)
   end
 
   describe 'POST #create' do
     context 'with valid params' do
       it 'updates the heartbeat' do
         expect do
-          post :create, params: { signal: valid_attributes }
+          post :create, params: valid_attributes
         end.to change(heartbeat, :last_signal_at)
           .and change(heartbeat, :last_signal_ok)
 
@@ -44,7 +36,7 @@ RSpec.describe SignalsController, type: :controller do # rubocop:disable Metrics
       end
 
       it 'renders a JSON response with the updated heartbeat' do
-        post :create, params: { signal: valid_attributes }
+        post :create, params: valid_attributes
         expect(response).to have_http_status(:created)
         expect(response.content_type).to eq('application/json')
       end
@@ -52,7 +44,7 @@ RSpec.describe SignalsController, type: :controller do # rubocop:disable Metrics
 
     context 'with invalid params' do
       it 'renders a JSON response with errors for the signal' do
-        post :create, params: { signal: invalid_attributes }
+        post :create, params: invalid_attributes
         expect(response).to have_http_status(:unprocessable_entity)
         expect(response.content_type).to eq('application/json')
       end
