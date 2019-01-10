9a971155d5ac49fe064baa2e44f09cefb7b3d3ee
Severin Räz <raez@puzzle.ch>
2019-01-10 16:50:21 +0100

    Always use HTTP 200 when showing checks, closes #1

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -8,8 +8,6 @@ class ChecksController < ApplicationController
 
   def show
     heartbeat = Heartbeat.find_by!(application: params[:application].to_s)
-    # :precondition_failed :failed_dependency
-    status = heartbeat.last_signal_ok? ? :ok : :not_acceptable
-    render json: heartbeat, status: status
+    render json: heartbeat
   end
 end
